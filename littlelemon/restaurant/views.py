e01a05f729761a91639e68dc7a82aaada898010f
Han Byul Moon <hanbyul.moon@bell.ca>
2024-07-26 10:48:59 -0400

    serializer and menu API

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Booking, Menu
+from .serializers import MenuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
